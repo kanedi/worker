375dc8faf710c21e8619c3a1384314cad7d9ea57
Martin Adiyono <martin@adiyono.com>
2014-07-20 14:31:04 +0700

    add currency name

--- models/MsCurrency.php
+++ models/MsCurrency.php
@@ -43,6 +43,32 @@ class MsCurrency extends \Phalcon\Mvc\Model
         ));
     }
 
+    public $currency_name = array(
+        'AUD' => "Australian dollar",
+        'BND' => "Brunei dollar",
+        'CAD' => "Canadian dollar",
+        'CHF' => "Swiss franc",
+        'CNY' => "Chinese yuan",
+        'DKK' => "Danish krone",
+        'EUR' => "EURO",
+        'GBP' => "Pound sterling",
+        'HKD' => "Hong Kong dollar",
+        'IDR' => "Indonesian rupiah",
+        'JPY' => "Japanese yen",
+        'KRW' => "South Korean won",
+        'KWD' => "Kuwaiti dinar",
+        'MYR' => "Malaysian ringgit",
+        'NOK' => "Norwegian krone",
+        'NZD' => "New Zealand dollar",
+        'PGK' => "Papua New Guinean kina",
+        'PHP' => "Philippine peso",
+        'SAR' => "Saudi riyal",
+        'SEK' => "Swedish krona",
+        'SGD' => "Singapore dollar",
+        'THB' => "Thai baht",
+        'USD' => "United States dollar"
+    );
+
     public function renew($currency_code, $rate){
         try{
             //Create a transaction manager
@@ -53,6 +79,7 @@ class MsCurrency extends \Phalcon\Mvc\Model
 
             $this->setTransaction($transaction);
             $this->code = $currency_code;
+            $this->name = $this->currency_name[$currency_code];
             $this->rate = $rate;
 
             if(!$this->save()){
